ac6e691e48e277ec47d038d81fd88584c106c0ea
Luan Lisboa Medeiros <109869151+medeirosluan@users.noreply.github.com>
2024-04-26 11:32:07 -0300

    atualização

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const frm = document.querySelector('form')
+const resp1 = document.querySelector('h3')
+const resp2 = document.querySelector('h4')
+
+frm.addEventListener('submit',(e) =>{
+    const nome = frm.inNome.value
+    const nota1 = Number.parseFloat(frm.inNotaUm.value)
+    const nota2 = Number.parseFloat(frm.inNotaDois.value)
+
+    const media = (nota1 + nota2) / 2
+
+    if (media >= 7 || media >= 5) {
+        resp1.innerText = `Media das notas ${media}`
+        resp2.innerText = `Parabens ${nome}! Você foi aprovado(a)`
+        resp2.style.color = 'blue'
+    } else {
+        resp2.innerText = `Desculpa ${nome}! Você foi reprovado(a)`
+        resp2.style.color = 'red'
+        
+    }
+
+    e.preventDefault()
+})
