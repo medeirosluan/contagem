8264aeceaa232fcfc01a430ecce505b32fb0ccbe
Luan Lisboa Medeiros <109869151+medeirosluan@users.noreply.github.com>
2024-04-27 11:40:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,11 +9,19 @@ frm.addEventListener('submit',(e) =>{
 
     const media = (nota1 + nota2) / 2
 
-    if (media >= 7 || media >= 5) {
+    if (media >= 7) {
         resp1.innerText = `Media das notas ${media}`
         resp2.innerText = `Parabens ${nome}! Você foi aprovado(a)`
         resp2.style.color = 'blue'
-    } else {
+    } else if (media >= 4) {
+        
+        resp2.innerText = `Atenção ${nome}. Você está em exame`
+        resp2.style.color = 'green'
+        
+    }
+    
+    
+    else {
         resp2.innerText = `Desculpa ${nome}! Você foi reprovado(a)`
         resp2.style.color = 'red'
         
