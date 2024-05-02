1c90752867f44a0e2aac43c25d0386cc913d6ab9
Luan Lisboa Medeiros <109869151+medeirosluan@users.noreply.github.com>
2024-05-02 11:21:21 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
-const frm = document.querySelector('form')
-const resp1 = document.querySelector('h3')
-const resp2 = document.querySelector('h4')
+const frm = document.querySelector('form');
+const resp = document.querySelector('h3');
 
-frm.addEventListener('submit',(e) =>{
-    const nome = frm.inNome.value
-    const nota1 = Number.parseFloat(frm.inNotaUm.value)
-    const nota2 = Number.parseFloat(frm.inNotaDois.value)
+frm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    const media = (nota1 + nota2) / 2
+    const respUser = Number(frm.inNumero.value);
+    const qtda = Number(frm.inQuantidade.value);
 
-    if (media >= 7) {
-        resp1.innerText = `Media das notas ${media}`
-        resp2.innerText = `Parabens ${nome}! Você foi aprovado(a)`
-        resp2.style.color = 'blue'
-    } else if (media >= 4) {
-        
-        resp2.innerText = `Atenção ${nome}. Você está em exame`
-        resp2.style.color = 'green'
-        
-    }
-    
-    
-    else {
-        resp2.innerText = `Desculpa ${nome}! Você foi reprovado(a)`
-        resp2.style.color = 'red'
-        
+    let resposta = '';
+
+    for (let i = qtda; i < respUser; i+= qtda) {
+        resposta += `${i}\n`;
+        resp.innerText = resposta;
     }
 
-    e.preventDefault()
-})
+});
